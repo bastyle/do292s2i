2b6a69f3f0dfb97cfdde970517938072de9dcc30
bebastia <bastian.bastias@consorcio.cl>
2020-09-11 20:53:52 +0000

    springboot openshift changes

---
+++ chapter_spring/lab/spring-rhoar-intro/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into fruit (name) values ('Cherry');
+insert into fruit (name) values ('Apple');
+insert into fruit (name) values ('Banana');
+insert into fruit (name) values ('Coconut');
+insert into fruit (name) values ('Pear');
+insert into fruit (name) values ('Pinneapple');
